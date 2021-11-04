1432f34f8ca0a94f26a5bfae5fedc6d76d279625
Artem Isaev <90577141+ArtemIsaevZXC@users.noreply.github.com>
2021-11-04 21:49:49 +0300

    Add files via upload

---
+++ web5/myscript.js
@@ -0,0 +1,19 @@
+console.log("Test");
+let num1 = document.getElementById("n1");
+let num2 = document.getElementById("n2");
+let result;
+let res = document.querySelector("#button");
+function multiply(){
+	 if((/^[1-9][0-9]*$/).test(num1.value) && (/^[1-9][0-9]*$/).test(num2.value))
+	{
+		result = num1.value*num2.value;
+        document.getElementById("result").innerHTML = result;
+	}
+	else{
+		alert("Введите корректные данные.");
+	}
+}
+window.addEventListener("DOMContentLoaded", function(event)
+{
+	res.addEventListener("click", multiply);
+});
